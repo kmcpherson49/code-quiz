405ca2b6b1a43cb9cb6a0f93830677ec3115bd8d
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-02-27 17:12:01 -0700

    html setup

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//GIVEN I am taking a code quiz WHEN I click the start button
+var buttonEL 
+//build start page in html with start button is an option
+
+//THEN a timer starts and I am presented with a question
+
+//WHEN I answer a question THEN I am presented with another question
+
+//WHEN I answer a question incorrectly THEN time is subtracted from the clock
+
+//WHEN all questions are answered or the timer reaches 0 THEN the game is over
+
+//WHEN the game is over THEN I can save my initials and score
+
+//quiz game that starts with a button click, and runs a timer
+//has 5 questions, multiple choice, eahc answer moved to the next question
+//incorrect answers detract time
+//shows highscores
+//uses localstorage to save high scores
+//boilerplate code for html, link to bootstrap and local css/js files
+//container where questions are displayed
+//eventListener, jQuery, setTime methods
+//array of questions move through dynamically; display: none, display:block;
+//onclick.getelementbyid
+//dynamic rendering
+//variable for questions and answers and correctness
+
+//card container w/ start menu when clicked use .empty to remove, then use jquery to create the next question
